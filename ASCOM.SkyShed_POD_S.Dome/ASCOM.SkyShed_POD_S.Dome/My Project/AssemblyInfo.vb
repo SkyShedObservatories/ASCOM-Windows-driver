be47e32d3841050f8a91efe4c628a458f7dbee37
SkyShedObservatories <110201763+SkyShedObservatories@users.noreply.github.com>
2023-01-07 21:00:08 -0500

    Additional protections
    The driver was renamed to get rid of dashes.
    Several changes for I/O protections
    Passes CONFORM

---
+++ ASCOM.SkyShed_POD_S.Dome/ASCOM.SkyShed_POD_S.Dome/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ASCOM SkyShed_POD_S.Dome")>
+<Assembly: AssemblyDescription("ASCOM Dome driver for SkyShed_POD_S")>
+<Assembly: AssemblyCompany("ASCOM Initiative")>
+<Assembly: AssemblyProduct("ASCOM Dome driver for SkyShed_POD_S")>
+<Assembly: AssemblyCopyright("Copyright © 2022, The ASCOM Intiative")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b87d4331-074e-473c-97e8-0e547f88ce07")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("6.5.1.0")>
+<Assembly: AssemblyFileVersion("6.5.1.0")>
